288fc715805bba1d5ecddf16ad9ca9970d84d09a
Дмитрий Чипижко <78786848+Chizad4o@users.noreply.github.com>
2021-06-06 17:18:22 +0300

    Чипижко 20-ИСТ2

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,26 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+BST<std::string>Tree;
+std::string result = "";
+char symbol;
+std::ifstream file(filename);
+if (!file) {
+std::cout << "File error!" << std::endl;
+return Tree;
+}
+while (!file.eof()) {
+symbol = file.get();
+if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')) {
+if (symbol >= 'A' && symbol <= 'Z') {
+symbol += 32;
+}
+result += symbol;
+} else if (result !="") {
+Tree.add(result);
+result = "";
+}
+}
+file.close();
+return Tree;
 }
